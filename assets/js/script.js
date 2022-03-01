0f568dc217a1a9f0f36512039b6672a2a65bfba0
auscarter17 <98193003+auscarter17@users.noreply.github.com>
2022-03-01 15:41:16 -0500

    added working timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
 const startEl = document.querySelector("#startscreen");
 
-const startButtonEl = document.querySelector("#btnBegin")
+const startButtonEl = document.querySelector("#btnBegin");
+
+const timerEl = document.querySelector("#timer")
+
+var interval;
+var count = 60;
+var time = null;
+
+function startTimer() {
+    time = setInterval(countDown, 1000);
+};
+
+function begin() {
+    countDown();
+    startTimer();
+};
+
+function countDown() {
+    document.getElementById("timer").innerHTML =
+    "<p> Time Left: " + count + " second(s) left</p>";
+    count--;
+};
+
+function hide(element) {
+    element.style.display = "none";
+};
+
+function show(element) {
+    element.style.display = "none";
+};
+
+startButtonEl.addEventListener("click", function() {
+    hide(startScreen);
+    begin();
+});
