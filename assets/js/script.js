b18f3c47f1b4628c83861694d0aef5a70d4b565e
auscarter17 <98193003+auscarter17@users.noreply.github.com>
2022-03-05 12:37:11 -0500

    added scoreboard to ending screen, added yellow highlight to answers on hover

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,33 @@ const timerEl = document.querySelector("#timer");
 
 const quizQuestionEl = document.querySelector("#quizQuestion");
 
+const scoreBoardEl = document.querySelector("#scoreboard");
+
+// starting point in question array to push up to next question in nextQuestion function
 var currentQuestion = 0;
 
+//stores initials of user
+var userName = "";
+
+// interval to set clock when quiz begins
 var interval;
+
+// 60 seconds for clock to begin countdown
 var count = 60;
+
+// time variable used to update timer in startTimer function
 var time = null;
+
+// will be used to add correct answers/remaining time to calculate final score
 userScore = 0;
 
+// used to store user answers during quiz
 var userInput = "";
 
+// stores high scores to be displayed
+let scores = JSON.parse(localStorage.getItem("userInfo")) || [];
+
+// array that stores questions to be used in the quiz
 var quizQuestion = [
     {
         question: "Which of the following is NOT a mathematic operator in JavaScript?",
@@ -72,6 +90,7 @@ function startTimer() {
     time = setInterval(countDown, 1000);
 };
 
+// starts countdown when player clicks start button
 function begin() {
     countDown();
     startTimer();
@@ -108,7 +127,8 @@ function fetchQuestion() {
     document.getElementById("solution3").innerHTML = quizQuestion[currentQuestion].answers.c;
     solution3.addEventListener("click", markAnswerC); 
 };
-    
+
+// marks user answer and sends to checkAnswer function
 function markAnswerA() {
     userInput = "a";
         checkAnswer();
@@ -144,13 +164,37 @@ function nextQuestion() {
     fetchQuestion();
 };
 
+// ending screen that shows final score and collects user info for scoreboard
 function endQuiz() {
     clearInterval(time);
     userScore = (userScore + count);
     alert("You reached the end!");
     alert("Your final score was " + userScore + ".");
-}
+    userName = prompt("Enter your initials.");
 
+    // combines score and initials for scoreboard
+    var userInfo = {
+        userName, 
+        userScore
+    };
+    scores.push(userInfo);
+    localStorage.setItem("userInfo", JSON.stringify(scores));
+
+    // hides last question
+    hide(quizQuestionEl);
+
+    // removes timer display and changes to scores banner
+    timerEl.innerHTML = "Previous Scores:";
+
+    // shows scoreboard
+    for (let i = 0; i < scores.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scores[i].userName}: ${scores[i].userScore}`;
+        scoreBoardEl.append(li);
+    }
+};
+
+// hides rules when quiz begins
 function hide(element) {
     element.style.display = "none";
 };
